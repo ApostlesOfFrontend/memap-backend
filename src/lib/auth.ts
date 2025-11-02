939d1148ab6a46b03b0f5d757cb5a274baba685c
Piotr Chmielowiec <chpiotr06@gmail.com>
2025-11-02 11:14:03 +0100

    feat: implemented basic better-auth routing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,16 @@
 import { betterAuth } from "better-auth";
 import { db } from "../db/index.js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { account, session, user, verification } from "../db/schemas/auth.js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
+    schema: {
+      user,
+      session,
+      account,
+      verification,
+    },
     provider: "pg",
   }),
   emailAndPassword: {
