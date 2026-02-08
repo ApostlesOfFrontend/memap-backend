63e93a4b78952a05847f2f90cfe431744ee9d02a
Piotr Chmielowiec <chpiotr06@gmail.com>
2026-02-08 17:23:38 +0100

    fix: attempt to fix r2 uploads

--- src/controllers/upload/single/init.ts
+++ src/controllers/upload/single/init.ts
@@ -25,9 +25,8 @@ export const initializeUpload = async (c: AuthContext): Promise<Response> => {
     new PutObjectCommand({
       Bucket: process.env.S3_BUCKET!,
       Key: s3Key,
-      ContentType: body.type,
     }),
-    { expiresIn: 3600, unhoistableHeaders: new Set(["Content-Type"]) },
+    { expiresIn: 3600, signableHeaders: new Set(["host"]) },
   );
 
   await db.insert(image).values({
