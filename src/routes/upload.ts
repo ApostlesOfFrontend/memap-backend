e8e29a3b379e749dffca2892d6c87608b6d57ced
Piotr Chmielowiec <chpiotr06@gmail.com>
2026-04-29 18:37:35 +0200

    feat: reworked trip images handling

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -3,12 +3,17 @@ import { AuthVariables } from "../types/auth-context";
 import { withAuthMiddleware } from "../middleware/withAuthMiddleware";
 import { initializeUpload } from "../controllers/upload/single/init";
 import { completeUpload } from "../controllers/upload/single/complete";
+import { initializeBatchUpload } from "@/controllers/upload/batch/init";
+import { completeBatchUpload } from "@/controllers/upload/batch/complete";
 
 export const uploadRoute = new Hono<{ Variables: AuthVariables }>().basePath(
-  "/upload"
+  "/upload",
 );
 
 uploadRoute.use("*", withAuthMiddleware);
 
 uploadRoute.post("/single/init", initializeUpload);
 uploadRoute.post("/single/confirm", completeUpload);
+
+uploadRoute.post("/batch/init", initializeBatchUpload);
+uploadRoute.post("/batch/confirm", completeBatchUpload);
